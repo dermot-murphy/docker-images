1b0bb8401f755f1bb517562370bdeb0500b3c920
Dermot Murphy <dmurphy@sensoteq.co.uk>
2021-08-06 14:48:51 +0100

    Working cmake

--- cmake-test/test.bat
+++ cmake-test/test.bat
@@ -1,10 +1,2 @@
-rem cmake -G Ninja ..
-rem cmake --build . --clean-first
-rem testApp
-docker run -it -v %cd%:/data test-arm astyle --version
-docker run -it -v %cd%:/data test-arm arm-none-eabi-gcc --version
-docker run -it -v %cd%:/data test-arm gcc --version
-docker run -it -v %cd%:/data test-arm cmake --version
-docker run -it -v %cd%:/data test-arm make --version
-docker run -it -v %cd%:/data test-arm ninja --version
-docker run -it -v %cd%:/data test-arm bash --version
+rd /S /Q builds
+docker run -it -v %cd%:/data canembed-arm-ubuntu pwd && mkdir builds && cd builds && cmake -G Ninja .. && cmake --build . && testApp
